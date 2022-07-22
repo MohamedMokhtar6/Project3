590dd58e55dfed2960256ffe967bc15d648b4002
Mohamed Mokhtar <Mohamed_201900731@fci.helwan.edu.eg>
2022-07-22 21:50:37 +0200

    .

--- javascript/master.js
+++
@@ -1,7 +0,0 @@
-let land =document.querySelector(".landing");
-let imgesArray = ["00.jpg","01.png","02.png","03.jpg"];
-setInterval(()=>{
-    let roundom=Math.floor(Math.random()*imgesArray.length);
-    land.style.backgroundImage= 'url("imgs/'+imgesArray[roundom]+'")';
-    
-},5000)
