bd2781d76e75b60e749a07f8752af14d519e75b3
MohamedMokhtar6 <96016335+MohamedMokhtar6@users.noreply.github.com>
2022-07-22 21:56:39 +0200

    Update master.js

--- javascript/master.js
+++ javascript/master.js
@@ -2,6 +2,6 @@ let land =document.querySelector(".landing");
 let imgesArray = ["00.jpg","01.png","02.png","03.jpg"];
 setInterval(()=>{
     let roundom=Math.floor(Math.random()*imgesArray.length);
-    land.style.backgroundImage= 'url("imgs/'+imgesArray[roundom]+'")';
+    land.style.backgroundImage= 'url("../imgs/'+imgesArray[roundom]+'")';
     
 },50000)
