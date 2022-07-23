2aa64fec7704960928a05f290e2a7326a5932aca
Mohamed Mokhtar <Mohamed_201900731@fci.helwan.edu.eg>
2022-07-23 21:31:44 +0200

    1

--- javascript/master.js
+++ javascript/master.js
@@ -1,7 +1,56 @@
-let land =document.querySelector(".landing");
-let imgesArray = ["00.jpg","01.png","02.png","03.jpg"];
-setInterval(()=>{
-    let roundom=Math.floor(Math.random()*imgesArray.length);
-    land.style.backgroundImage= 'url("../imgs/'+imgesArray[roundom]+'")';
+let maincolor=localStorage.getItem("color-op");
+if(maincolor!=null){
+    document.documentElement.style.setProperty("--main-color",maincolor);
+    document.querySelectorAll(".option-box ul li").forEach(element =>{
+
+        element.classList.remove("active");
+        if(element.dataset.color===maincolor){
+            element.classList.add("active");
+        }
+    });
+
+}
+document.querySelector(".icon .fa-gear").onclick=function(){
+    this.classList.toggle("fa-spin");
+    document.querySelector(".setting").classList.toggle("open");
     
-},5000)
+};
+const colorlis=document.querySelectorAll(".links li");
+
+colorlis.forEach(li =>{
+    li.addEventListener("click",(e)=>{
+        document.documentElement.style.setProperty("--main-color",e.target.dataset.color);
+        localStorage.setItem("color-op",e.target.dataset.color);
+        e.target.parentElement.querySelectorAll(".active").forEach(element =>{
+
+            element.classList.remove("active");
+        });
+        e.target.classList.add("active");
+        
+    });
+});
+let imgesArray = ["../imgs/00.jpg","../imgs/01.png","../imgs/02.png","../imgs/03.jpg"];
+let land =document.querySelector(".landing");
+const images=document.querySelectorAll(".imges li");
+images.forEach(li=>{
+    li.addEventListener("click",(e)=>{
+        land.style.backgroundImage='url('+imgesArray[e.target.dataset.num]+')';
+        localStorage.setItem("background",e.target.dataset.num);
+
+    });
+});
+let back=localStorage.getItem("background");
+if(back!==null){
+
+    land.style.backgroundImage='url('+imgesArray[localStorage.getItem("background")]+')';
+
+}
+
+
+
+
+
+
+
+
+
